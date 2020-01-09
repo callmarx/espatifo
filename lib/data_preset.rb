d7cb4f0d16b3a51afe54a8229c1e6673edd1367c
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2020-01-09 15:11:38 -0300

    separado o cálculo de média em DataAverage

--- lib/data_preset.rb
+++ lib/data_preset.rb
@@ -74,37 +74,5 @@ module DataPreset
     end
     return [true, query_array.join(" AND ")]
   end
-
-  def self.get_average(filtered_list, average_params)
-    result_average ={}
-    average_params.each do |key|
-      calc = calc_average(key, filtered_list)
-      result_average[key] = {
-            "total_NA": calc[0],
-            "total_non_NA": calc[1],
-            "average": calc[2]
-      }
-    end
-    result_average
-  end
-
-  def self.calc_average(key, filtered_list)
-    total_non_NA = 0
-    total_sum = 0.0
-    total_NA = 0
-    filtered_list.each do |entry|
-      if entry.row[key] == "NA"
-        total_NA += 1
-      else
-        total_non_NA += 1
-        if entry.row[key].is_a? Numeric
-          total_sum += entry.row[key].to_f
-        else # Se não é numerico não tem como calcular a média :-)
-          return [nil, nil, "Non Numeric field"]
-        end
-      end
-    end
-    return [total_NA, total_non_NA, total_sum/total_non_NA.to_f]
-  end
 end
 
